d7c8347d2fbb00d172f438ff1e2f4713af4abe92
Anton <anton.naumenko.a@gmail.com>
2024-01-05 23:34:20 +0100

    6th

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
-print ("Hello World")
+print ("Hello World", end=" ")
 
 a = 0.2 + 0.1
 print(a)
@@ -7,5 +7,9 @@ print(a)
 s1 = "Hello"
 s2 = "World!"
 joined_string = f"{s1} and {s2}"
+
+'''
+Comment tes
+'''
 
 print(joined_string)
